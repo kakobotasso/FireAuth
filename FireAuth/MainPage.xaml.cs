7b7c466d2ed6129b9dfd374e9b93cd5a9e64e6e3
Kako Botasso <kakodev@gmail.com>
2019-06-14 20:25:54 -0300

    Add layout do MainPage

--- FireAuth/MainPage.xaml.cs
+++ FireAuth/MainPage.xaml.cs
@@ -8,14 +8,16 @@ using Xamarin.Forms;
 
 namespace FireAuth
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void LoginClicked(object sender, EventArgs e) 
+        {
         }
     }
 }
