512a3cd3a6d2a0ca02811262f973a423a0f563d9
Kako Botasso <kakodev@gmail.com>
2019-06-21 23:24:18 -0300

    Add iOS implementation

---
+++ FireAuth.iOS/AuthIOS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using FireAuth;
+using FireAuth.iOS;
+using Xamarin.Forms;
+using Firebase.Auth;
+
+[assembly: Dependency(typeof(AuthIOS))]
+namespace FireAuth.iOS
+{
+    public class AuthIOS : IAuth
+    {
+        public async Task<string> LoginWithEmailPassword(string email, string password)
+        {
+            try
+            {
+                var user = await Auth.DefaultInstance.SignInWithPasswordAsync(email, password);
+                return await user.User.GetIdTokenAsync();
+            }
+            catch (Exception e)
+            {
+                return "";
+            }
+
+        }
+    }
+}
